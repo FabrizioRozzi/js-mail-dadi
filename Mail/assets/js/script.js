6f64a54034e6538c3e197e8dc270147f043b8865
FabrizioRozzi <rozzi458@gmail.com>
2021-04-08 15:58:14 +0200

    finito

--- Mail/assets/js/script.js
+++ Mail/assets/js/script.js
@@ -11,14 +11,21 @@ var email = [
   "Bimb3tt0-_paxxerello89@netlog.com"
 ];
 
-var emailCiclo= "";
+
 var tuaEmail = prompt("Inserire la propria e-mail:");
 
 for(var i = 0; i < email.length; i++){
-  
+  console.log(i);
   if(email[i] === tuaEmail ){
-    document.getElementById('risposta').innerHTML = "L' email:" + " " + tuaEmail + " " + "é presente nel nostro database."
+   
+    document.getElementById('risposta').innerHTML = "L' email:" + " " + tuaEmail + " " + "é presente nel nostro database.";
+    break;
+
+  }else if(email[i] != tuaEmail){
+    
+    document.getElementById('risposta').innerHTML = "L' email:" + " " + tuaEmail + " " + "non é presente nel nostro database.";
+   
   }
+   
 }
 
-document.getElementById('risposta').innerHTML = "L' email:" + " " + tuaEmail + " " + "non é presente nel nostro database."
